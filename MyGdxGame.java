2dc8efeda1e021bc806e455ba631c42bf5873f46
Laladehulu <39814387+laladehulu@users.noreply.github.com>
2021-02-27 11:35:15 -0800

    Update MyGdxGame.java

--- MyGdxGame.java
+++ MyGdxGame.java
@@ -50,7 +50,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT|GL20.GL_DEPTH_BUFFER_BIT);//bitwise 'or', basically combine the flags on two binary sequence.not rlly related much to game programming
 		camera.update();//just call it
 		batch.begin(camera);//positioning and probably screen 
 		batch.render(boxInstance);
